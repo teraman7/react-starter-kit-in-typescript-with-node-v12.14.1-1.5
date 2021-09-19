1c8ce339c3ea5a4c83aaf97dd3695c9a48401c9d
teraman7 <puman0815@gmail.com>
2021-09-19 16:30:10 +0900

    lesson 2021-09-19

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Counter from './Counter';
+// import Counter from './Counter';
+import CounterWithReducer from './CounterWithReducer';
 
 interface AppProps {
   message?: string;
@@ -7,7 +8,7 @@ interface AppProps {
 
 const App: React.FunctionComponent<AppProps> = ({ message }) => {
   return(
-    <div><Counter /></div>
+    <div><CounterWithReducer /></div>
   );
 };
 
