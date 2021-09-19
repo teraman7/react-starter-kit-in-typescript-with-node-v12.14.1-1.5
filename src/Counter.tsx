59064b7cc552c0c87116e181651b3df40387037d
teraman7 <puman0815@gmail.com>
2021-09-19 16:10:32 +0900

    lesson 2021-09-19

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -9,11 +9,17 @@ const Counter: React.FunctionComponent<{}> = () => {
     const decrement = () => {
         setValue((prevState) => prevState - 1);
     };
-    
+
     const renderTimes = useRef<number>(0);
     useEffect(() => {
         renderTimes.current = renderTimes.current + 1;
     });
+
+    const ref = useRef<HTMLInputElement>(null!);
+    const foucusInput = () => {
+        ref.current.focus();
+    };
+
 
     return (
         <div>
@@ -21,6 +27,8 @@ const Counter: React.FunctionComponent<{}> = () => {
             <button onClick={ increment }>+1</button>
             <button onClick={ decrement }>-1</button>
             <div>This component was re-rendered {renderTimes.current} times!</div>
+            <input ref={ref} type="text" />
+            <button onClick={foucusInput}>Click Me!</button>
         </div>
     );
 };
